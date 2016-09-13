ef96dc24c351423cad318da46be38e4c182cf45c
Eugene Scherba <escherba@gmail.com>
2016-09-13 12:40:07 -0700

    add version string

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ CXXFLAGS = u"""
 -Wno-unused-function
 """.split()
 
-VERSION = '0.2.0'
+VERSION = '0.0.1'
 URL = 'https://github.com/escherba/clustering-metrics'
 
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,6 @@ import unittest
 import numpy as np
 from clustering_metrics.fent import minmaxr
 from clustering_metrics.utils import sort_by_length
-from clustering_metrics import create_sig_selectors
 
 
 class TestUtils(unittest.TestCase):
@@ -27,36 +26,6 @@ class TestUtils(unittest.TestCase):
         test_case = ["abraca", "abracadabra", "a", "aba"]
         result = sort_by_length(test_case)
         self.assertEqual(list(result), ["abracadabra", "abraca", "aba", "a"])
-
-    def test_create_sig_selectors1(self):
-        """Test non-overlapping selectors"""
-        sig = range(9)
-        sig_res = create_sig_selectors(9, 3, "a0")
-        selectors = sig_res
-        selected = []
-        for _, selector in selectors:
-            selected.append(selector(sig))
-        self.assertListEqual(selected, [(0, 1, 2),
-                                        (3, 4, 5),
-                                        (6, 7, 8)])
-
-    def test_create_sig_selectors2(self):
-        """Test overlapping selectors"""
-        sig = range(4)
-        sig_res = create_sig_selectors(4, 2, "a1")
-        selectors = sig_res
-        selected = []
-        for _, selector in selectors:
-            selected.append(selector(sig))
-        self.assertListEqual(selected, [(0, 1),
-                                        (0, 3),
-                                        (1, 2),
-                                        (2, 3)])
-
-    def test_create_sig_selectors3(self):
-        """Number of selectors must match n choose k"""
-        selectors = create_sig_selectors(8, 3, "a3")
-        self.assertEqual(len(selectors), 56)
 
 
 if __name__ == '__main__':
