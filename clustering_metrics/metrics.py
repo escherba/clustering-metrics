a1ff4df1d3dfef1751aa75b3df921be7475a807f
Eugene Scherba <escherba@gmail.com>
2020-12-25 23:02:25 -0800

    add text

--- clustering_metrics/metrics.py
+++ clustering_metrics/metrics.py
@@ -1775,11 +1775,11 @@ class ConfusionMatrix2(ContingencyTable, OrderedCrossTab):
             1.0
 
         Loevinger's coefficient has a unique property: all two-way correlation
-        coefficients on a 2x2 table that are in L-family (including Kappa and
-        Matthews' correlation coefficient) become Loevinger's coefficient after
-        normalization by maximum value [1]_. However, this measure is not
-        symmetric: when :math:`ad < bc`, it does not have a lower bound. For an
-        equivalent symmetric measure, use Cole coefficient.
+        coefficients on a 2x2 table that are in L-family (including Precision,
+        Kappa and Matthews' correlation coefficient) become Loevinger's
+        coefficient after normalization by maximum value [1]_. However, this
+        measure is not symmetric: when :math:`ad < bc`, it does not have a
+        lower bound. For an equivalent symmetric measure, use Cole coefficient.
 
         See Also
         --------
