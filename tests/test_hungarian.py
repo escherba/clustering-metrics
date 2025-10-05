5a2c8f3ad0e6ca929b6e41c4acf7807c10cd9955
Eugene <escherba@gmail.com>
2025-10-04 17:49:38 -0700

    (2)

--- tests/test_hungarian.py
+++ tests/test_hungarian.py
@@ -103,7 +103,4 @@ class HungaritanTest(unittest.TestCase):
         assert_raises(ValueError, linear_sum_assignment, [1, 2, 3])
 
         C = [[1, 2, 3], [4, 5, 6]]
-        assert_array_equal(linear_sum_assignment(C),
-                           linear_sum_assignment(np.asarray(C)))
-        assert_array_equal(linear_sum_assignment(C),
-                           linear_sum_assignment(np.matrix(C)))
+        assert_array_equal(linear_sum_assignment(C), linear_sum_assignment(np.asarray(C)))
